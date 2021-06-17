71cfc41e5554cbc306effdac0eb5ef96fbab57ed
hpc-0235 <hpc-0235@wahab-01.ib.cluster>
2021-06-17 02:41:36 -0400

    stopping for tonight finally

--- job_script_2.sh
+++ job_script_2.sh
@@ -11,7 +11,8 @@
 enable_lmod
 module load container_env tensorflow-gpu/2.2.0
 
-srun crun.tensorflow-gpu python -m visdom.server &
-srun crun.tensorflow-gpu python3 main.py --dataset_dir skeleton_convert/ --mode train --model_name HCN --dataset_name NTU-RGB-D-CV --num 01
+crun.tensorflow-gpu python -m visdom.server &
+crun.tensorflow-gpu python3 main.py --dataset_dir skeleton_convert/ --mode train --model_name HCN --dataset_name NTU-RGB-D-CV --num 01
 
 ## Need to figure out how to access visdom here!! When I can access it remotely, the HCN-PyTorch cannot. When I can't access it remotely, the HCN-PyTorch can. Something weird with GPU partition connections?
+## Can only access visdom when NOT running on a gpu partition. Meanwhile, HCN-PyTorch can only access visdom when they are running on the SAME NODE within the gpu partition, or at least best I can tell.
