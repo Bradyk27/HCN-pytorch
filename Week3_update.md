61439339da87a83e481afee605e79b0ba142c019
Brady <bradyakruse@gmail.com>
2021-06-30 15:05:31 -0500

    split into frames of 10 a piece

---
+++ Week3_update.md
@@ -0,0 +1,15 @@
+**This Week**
+
+1. Successfully relabled and converted our data to work with the HCN-PyTorch model, including adapting from Action ID to Person ID
+2. Worked with Meghan, Joe, Alex on ways to further "stretch" our data
+3. Received and began reading GAN documentation
+
+**Next Week**
+
+1. Try the various methods we discussed to stretch data
+2. Review GAN code and become adept with it.
+
+**Future**
+1. Continue meeting with Dr. Khan, Alex, Joe, Meghan to help decide further progress for the project.
+
+6/25/21 bk
--- data_and_visualization/lidar_mocap/lidar_to_skel.py
+++ data_and_visualization/lidar_mocap/lidar_to_skel.py
@@ -2,88 +2,88 @@
 #Plenty of room for code cleaning & formatting, including opening files up and down. Nonetheless, this works.
 import numpy as np
 import os
+import math
 
 csv_files = [f for f in os.listdir('./') if f[len(f)-3::] == "csv"]
 csv_files.sort()
-video_labels = []
-for i in range(1, len(csv_files)+1):
-    if len(str(i))==1:
-        video_labels.append('video0'+str(i))
+subject_labels = {}
+for file in csv_files:
+    subject_id = file[file.find('subject')+7:file.find('subject')+9]
+    if subject_id not in subject_labels:
+        subject_labels.update({subject_id:[file]})
     else:
-        video_labels.append('video'+str(i))
+        subject_labels[subject_id].append(file)
 
-video_labels = video_labels[0::2]
+for lab in subject_labels:
+    video_lab = 1
+    for file in subject_labels[lab]:
+        with open(file, 'r') as f:
+            total_num_frames = len(f.readlines())
 
-for lab in video_labels:
-    for file in csv_files:
-        if file.find(lab) != -1:
-            working_files = [file, csv_files[csv_files.index(file)+1]]
-            subject_id = file[file.find('subject')+7:file.find('subject')+9]
+        with open(file, 'r') as f:
+            for i in range(math.floor(total_num_frames / 10)):
+                with open('video'+str(video_lab) + 'subject' + lab + '.skeleton', 'a+') as n:
+                    num_bodies = 1
+                    num_frames = 10
+                    trackingID = int(lab)
+                    clippedEdges = 0
+                    hlc = 0
+                    hls = 0
+                    hrc = 0
+                    hrs = 0
+                    isrest = 0
+                    lean1 = 0
+                    lean2 = 0
+                    ts = 2
+                    num_joints = 13
+                    
+                    n.write(str(num_frames)+'\n')
+                    header = [trackingID, clippedEdges, hlc, hls, hrc, hrs, isrest, lean1, lean2, ts]
 
-            for each in working_files:
-                with open(each, 'r') as f:
-                    num_frames = len(f.readlines())
-                with open(each, 'r') as f:
-                    with open('video0'+str(working_files.index(each)+1) + 'subject' + subject_id + '.skeleton', 'a+') as n:
-                        num_bodies = 1
-                        trackingID = int(subject_id)
-                        clippedEdges = 0
-                        hlc = 0
-                        hls = 0
-                        hrc = 0
-                        hrs = 0
-                        isrest = 0
-                        lean1 = 0
-                        lean2 = 0
-                        ts = 2
-                        num_joints = 13
-                        
-                        n.write(str(num_frames)+'\n')
-                        header = [trackingID, clippedEdges, hlc, hls, hrc, hrs, isrest, lean1, lean2, ts]
-
-                        n.write(str(num_bodies)+'\n')
-                        for item in header:
-                            if header.index(item)==len(header)-1:
-                                n.write(str(item))
-                            else:
-                                n.write(str(item)+' ')
-                        n.write('\n'+str(num_joints)+'\n')
+                    n.write(str(num_bodies)+'\n')
+                    for item in header:
+                        if header.index(item)==len(header)-1:
+                            n.write(str(item))
+                        else:
+                            n.write(str(item)+' ')
+                    n.write('\n'+str(num_joints)+'\n')
 
 
-                        for i in range(num_frames):
-                            line = f.readline().strip('\n').split(',')
-                            for j in range(0, (num_joints*3),3):
-                                x = line[j]
-                                y = line[j+1]
-                                z = line[j+2]
-                                depthX = 0
-                                depthY = 0
-                                colorX = x
-                                colorY = y
-                                oW = 0
-                                oX = 0
-                                oY = 0
-                                oZ = 0
-                                ts = 2
+                    for i in range(10):
+                        line = f.readline().strip('\n').split(',')
+                        for j in range(0, (num_joints*3),3):
+                            x = line[j]
+                            y = line[j+1]
+                            z = line[j+2]
+                            depthX = 0
+                            depthY = 0
+                            colorX = x
+                            colorY = y
+                            oW = 0
+                            oX = 0
+                            oY = 0
+                            oZ = 0
+                            ts = 2
 
-                                skel_line = [x,y,z,depthX,depthY,colorX,colorY,oW,oX,oY,oZ,ts]
+                            skel_line = [x,y,z,depthX,depthY,colorX,colorY,oW,oX,oY,oZ,ts]
 
-                                for item in skel_line:
-                                    if item == '-0':
-                                        item = '0'
-                                    if skel_line.index(item)==len(skel_line)-1:
-                                        n.write(str(item))
-                                    else:
-                                        n.write(str(item) + ' ')
-                                n.write('\n')
+                            for item in skel_line:
+                                if item == '-0':
+                                    item = '0'
+                                if skel_line.index(item)==len(skel_line)-1:
+                                    n.write(str(item))
+                                else:
+                                    n.write(str(item) + ' ')
+                            n.write('\n')
 
-                            if i != num_frames-1:
-                                n.write(str(num_bodies)+'\n')
-                                
-                                for item in header:
-                                    if header.index(item)==len(header)-1:
-                                        n.write(str(item))
-                                    else:
-                                        n.write(str(item) + ' ')
-                                n.write('\n'+str(num_joints)+'\n')
-
+                        if i != num_frames-1:
+                            n.write(str(num_bodies)+'\n')
+                            
+                            for item in header:
+                                if header.index(item)==len(header)-1:
+                                    n.write(str(item))
+                                else:
+                                    n.write(str(item) + ' ')
+                            n.write('\n'+str(num_joints)+'\n')
+                video_lab+=1
+        #dawgs by a million
