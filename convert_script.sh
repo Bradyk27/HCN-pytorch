0a22b49cea79d46ebd8d23dcfa13ae3b0b0a5de2
hpc-0235 <hpc-0235@wahab-01.ib.cluster>
2021-07-05 15:44:02 -0400

    moving to Windows, need to get this stupid visdom server going

--- convert_script.sh
+++ convert_script.sh
@@ -1,12 +1,13 @@
-#!/bin/bash                                                                                                                                                            
-#SBATCH -N 3                                                                                                                                                                      
-#SBATCH --job-name=convert_HCN                                                                                                                                                      
-#SBATCH --output=convert_HCN.log                                                                                                                                                    
-#SBATCH --mail-type=ALL                                                                                                                                                             
-#SBATCH --mail-user=bradyakruse@gmail.com                                                                                                                                          
+#!/bin/bash
+
+#SBATCH -N 3
+#SBATCH --job-name=convert_HCN
+#SBATCH --output=convert_HCN.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=bradyakruse@gmail.com
 
 enable_lmod
 module load container_env tensorflow-gpu/2.2.0
 
-crun.tensorflow-gpu python3 ./feeder/bk_gendata.py --data_path lidar_mocap --out_folder lidar_mocap_convert
+crun.tensorflow-gpu python3 ./feeder/ntu_gendata.py --data_path skeletons --out_folder skeletons_convert
 
--- train_script.sh
+++ train_script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#SBATCH -p gpu
-#SBATCH --gres gpu:1
 #SBATCH -N 1
 #SBATCH --job-name=train_HCN
 #SBATCH --output=train_HCN.log
@@ -11,8 +9,8 @@
 enable_lmod
 module load container_env tensorflow-gpu/2.2.0
 
-crun.tensorflow-gpu python -m visdom.server &
-crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 01
+crun.tensorflow-gpu python -m visdom.server -p 9005
+## crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 01
 
 ## Need to figure out how to access visdom here!! When I can access it remotely, the HCN-PyTorch cannot. When I can't access it remotely, the HCN-PyTorch can. Something weird with GPU partition connections?
 ## Can only access visdom when NOT running on a gpu partition. Meanwhile, HCN-PyTorch can only access visdom when they are running on the SAME NODE within the gpu partition, or at least best I can tell.
