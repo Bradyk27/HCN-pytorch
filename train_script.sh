9185abb179da40a8da64fbb428000ddf882b9238
hpc-0235 <hpc-0235@wahab-01.ib.cluster>
2021-07-08 04:16:26 -0400

    REAL REAL REAL CHECKPOINT. STUPID FOLDER NAMING

--- train_script.sh
+++ train_script.sh
@@ -12,6 +12,15 @@ enable_lmod
 module load container_env tensorflow-gpu/2.2.0
 
 crun.tensorflow-gpu python -m visdom.server &
+crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 01
+crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 02
+crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 03
+crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 04
+crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 05
+crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 07
+crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 08
+crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 09
+crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 10
 crun.tensorflow-gpu python3 main.py --dataset_dir lidar_mocap_split_convert/ --mode train --model_name HCN --dataset_name lidar_mocap --num 11
 
 ## Need to figure out how to access visdom here!! When I can access it remotely, the HCN-PyTorch cannot. When I can't access it remotely, the HCN-PyTorch can. Something weird with GPU partition connections?
