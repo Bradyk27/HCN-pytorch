9dd03777df41f3c9d54583b33ac8cdb1d091b336
hpc-0235 <hpc-0235@wahab-01.ib.cluster>
2021-07-12 17:43:51 -0400

    accidentally deleted my scripts cuz I'm a dumbass

---
+++ lidar_mocap/even_split.py
@@ -0,0 +1,26 @@
+import numpy as np
+import os
+import math
+import random
+
+max_num = 58
+
+skel_files = [f for f in os.listdir('./') if f[len(f)-8::] == "skeleton"]
+skel_files.sort()
+subject_labels = {}
+for file in skel_files:
+    subject_id = file[file.find('subject')+7:file.find('subject')+9]
+    if subject_id not in subject_labels:
+        subject_labels.update({subject_id:[file]})
+    else:
+        subject_labels[subject_id].append(file)
+
+selected_files = []
+for lab in subject_labels:
+    random.shuffle(subject_labels[lab])
+    for f in subject_labels[lab][:max_num]:
+        selected_files.append(f)
+
+for f in os.listdir('./'):
+    if f not in selected_files:
+        os.remove(f)
---
+++ lidar_mocap/train_eval_split.py
@@ -0,0 +1,14 @@
+import numpy as np
+import os
+import math
+import random
+
+video_id = 999
+csv_files = [f for f in os.listdir('./') if f[len(f)-8::] == "skeleton"]
+random.shuffle(csv_files)
+eval_files = csv_files[0:math.floor(len(csv_files)/3)]
+for file in eval_files:
+    subject_id = file[file.find('subject')+7:file.find('subject')+9]
+    os.rename(file, "video" + str(video_id) + "subject" + subject_id + ".skeleton")
+    video_id += 1
+print(video_id)
