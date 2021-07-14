8df3b196627d084d7d542d42a833a3ef949b5738
hpc-0235 <hpc-0235@wahab-01.ib.cluster>
2021-07-14 17:09:44 -0400

    fixed train_eval script to make sure everyone is represented

--- lidar_mocap/train_eval_split.py
+++ lidar_mocap/train_eval_split.py
@@ -3,12 +3,23 @@ import os
 import math
 import random
 
-video_id = 999
-csv_files = [f for f in os.listdir('./') if f[len(f)-8::] == "skeleton"]
-random.shuffle(csv_files)
-eval_files = csv_files[0:math.floor(len(csv_files)/4)] #This determines split!
-for file in eval_files:
-    subject_id = file[file.find('subject')+7:file.find('subject')+9]
-    os.rename(file, "video" + str(video_id) + "subject" + subject_id + ".skeleton")
-    video_id += 1
-print(video_id)
+pass_fail = 0
+while not pass_fail:
+    video_id = 999
+    csv_files = [f for f in os.listdir('./') if f[len(f)-8::] == "skeleton"]
+    random.shuffle(csv_files)
+    eval_files = csv_files[0:math.floor(len(csv_files)/4)] #This determines split!
+    for file in eval_files:
+        subject_id_list = ['01', '02', '03', '04', '05', '06', '07', '08' ,'09', '10']
+        subject_id = file[file.find('subject')+7:file.find('subject')+9]
+        try:
+            subject_id_list.pop(subject_id_list.index(subject_id))
+        except:
+            pass
+    if len(subject_id_list) == 0:
+        for file in eval_files:
+            subject_id = file[file.find('subject')+7:file.find('subject')+9]            
+            os.rename(file, "video" + str(video_id) + "subject" + subject_id + ".skeleton")
+            video_id += 1
+        pass_fail = 1
+
